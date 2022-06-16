21c6773ef7e99499b0291b040ffb0b3a9fcd0d0f
Sadık Can Acar <77164535+scanacar@users.noreply.github.com>
2022-06-16 23:13:17 +0300

    Business and Presentation Layers added

---
+++ PortfolioWebsite/BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager : IGenericService<Feature>
+    {
+        IFeatureDAL _featureDAL;
+
+        public FeatureManager(IFeatureDAL featureDAL)
+        {
+            _featureDAL = featureDAL;
+        }
+
+        public void TAdd(Feature parameter)
+        {
+            _featureDAL.Insert(parameter);
+        }
+
+        public void TDelete(Feature parameter)
+        {
+            _featureDAL.Delete(parameter);
+        }
+
+        public Feature TGetById(int id)
+        {
+            return _featureDAL.getById(id);
+        }
+
+        public List<Feature> TGetList()
+        {
+            return _featureDAL.getList();
+        }
+
+        public void TUpdate(Feature parameter)
+        {
+            _featureDAL.Update(parameter);
+        }
+    }
+}
